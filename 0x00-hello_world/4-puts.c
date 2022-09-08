bbfabae9e5f2db196a87875eb97411b186b49f70
ermitse <ermiastse@gmail.com>
2022-09-08 14:19:20 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string;	/*declaring a char*/
+
+	strcpy(string, "Programming is like building a multilingual puzzle");
+
+	puts(string);	/*prints the char stored in string*/
+
+	    return (0);
+}
