cfc787c6245893018b1fa1895123218331b9af72
ermitse <ermiastse@gmail.com>
2022-09-09 15:05:51 -0700

    d

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,17 +3,18 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (success)
+ * Return: Always 0 (success)
  *
  */
 
 int main(void)
 {
 	int n;
+
 	for (n = 0; n <= 9; n++)
 	{
 	putchar(n + '0');
-	if(n != 9)
+	if (n != 9)
 	{
 	putchar(',');
 	putchar(' ');
