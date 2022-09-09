75fd7e20a3a40fca4a467622c07560c94e3a9808
ermitse <ermiastse@gmail.com>
2022-09-09 15:24:21 -0700

    f

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+
+	for (m = 0; m <= 9; m++)
+	{
+	if (n != m)
+	{
+	putchar(nm);
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	return (0);
+}
