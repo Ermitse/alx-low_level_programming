ff8a012a13f1b939f75f8450b05d9f18016f1682
ermitse <ermiastse@gmail.com>
2022-09-09 12:04:49 -0700

    t

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char alpa;
+
+	for (alpa = 'a'; alpa <= 'z'; alpa++)
+	{
+	putchar(alpa"\n");
+	}
+	return (0);
+}
