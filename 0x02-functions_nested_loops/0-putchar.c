57486a78dec175ae4612bf9db2f734c947864635
ermitse <ermiastse@gmail.com>
2022-09-13 09:14:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -10,14 +10,14 @@
 int main(void)
 {
 
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
