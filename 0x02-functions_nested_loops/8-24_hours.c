b7814011d12c395c22f4557db8577b71b3bd5572
ermitse <ermiastse@gmail.com>
2022-09-13 16:39:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,4 +9,17 @@
 void jack_bauer(void)
 {
 	
-	for ()
+	int m, n;
+	for (n = 0; n < 24; n++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
