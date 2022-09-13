5fcef5e7db4b66349ca0d4ab5c1a40008add31f0
ermitse <ermiastse@gmail.com>
2022-09-13 12:01:55 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: integer input
+ *
+ * Return: 1 or 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+	if (i == c)
+	{
+	return (1);
+	}
+	}
+	return (0);
+}
