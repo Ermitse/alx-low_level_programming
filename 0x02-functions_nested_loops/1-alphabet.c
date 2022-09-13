67f27278ed06fe6d991c25dc5de99eab9b0a2aff
ermitse <ermiastse@gmail.com>
2022-09-13 09:37:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	for (char lc = 'a' ; lc <= 'z'; lc++)
+	_putchar(lc);
+	_putchar(lc);
+	return (0);
+}
