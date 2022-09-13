d4feafd08396954a2786093f33277bf031a0cc1b
ermitse <ermiastse@gmail.com>
2022-09-13 15:56:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,21 +10,23 @@ void print_to_98(int n)
 {
 	int i;
 
-	if(n < 98)
+	if (n < 98)
 	{
 	for (i = n; i <= 98; i++)
-	}
+
 	else if (n > 98)
 	{
-	for (i = n; i >=98; i--)
-	}
+	for (i = n; i >= 98; i--)
+
 	else
 	{
 	i = n;
-	}
+
 		_putchar(i + '0');
 		_putchar(',');
 		_putchar(' ');
 		_putchar('\n');
-	
+	}
+	}
+	}
 }
