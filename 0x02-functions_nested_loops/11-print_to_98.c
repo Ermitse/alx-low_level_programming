54919b079cf33b3250454ba98999ab4696c1f226
ermitse <ermiastse@gmail.com>
2022-09-13 16:23:03 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,21 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-
-	for (i = n; i <= 98; i++)
-	_putchar(i + '0');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('\n');
-	if (n > 98)
-	for (i = n; i >= 98; i--)
-
+	if (n < 98)
 	{
-	_putchar(i + '0');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('\n');
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
 }
+printf("98\n");
