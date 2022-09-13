55a00f358ca6ec00e33b530b2507ba85a787a991
ermitse <ermiastse@gmail.com>
2022-09-13 14:13:14 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @l: last digit
+ *
+ * Return: 0 or 1
+ *
+ */
+int print_last_digit(int l)
+{
+	int n;
+
+	n = j % 10;
+	return (n);
+}
