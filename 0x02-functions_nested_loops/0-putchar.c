b10b95ba57fb1c8e858e8478bcbf4b89cf6ed9ea
ermitse <ermiastse@gmail.com>
2022-09-13 08:24:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char c = '_putchar';
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
