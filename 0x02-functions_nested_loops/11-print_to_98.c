f757238ad8c217f498de89e0eae2814995fadab8
ermitse <ermiastse@gmail.com>
2022-09-13 15:33:29 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print nms
+ *@n: input
+ * Return: ret
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+	}
+}
