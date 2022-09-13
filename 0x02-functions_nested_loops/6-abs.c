0a1f10a6b7343238899bc0feb649597c6a58f297
ermitse <ermiastse@gmail.com>
2022-09-13 13:14:06 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @a: input integer
+ *
+ * Return: value
+ *
+ */
+int _abs(int a)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
