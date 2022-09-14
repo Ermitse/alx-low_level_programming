a3edfa589aa87239cc6245b57796cd211c3f1abd
ermitse <ermiastse@gmail.com>
2022-09-14 14:17:34 -0700

    fibu

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * return: 0
+ * Return: 0
  *
  */
 int main(void)
 {
 	unsigned long f1 = 0, f2 = 1, f3 = 0, n = 0;
-	
+
 	while (f3 <= 4000000)
 	{
 		f3 = f1 + f2;
