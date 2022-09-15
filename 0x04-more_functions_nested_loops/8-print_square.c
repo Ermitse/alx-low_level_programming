b466e5c82e77697f5a556397ccc007d3adf15ee7
ermitse <ermiastse@gmail.com>
2022-09-15 16:08:45 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,14 +5,14 @@
  * @n: int
  * Return: no
  */
- void print_square(int n)
+void print_square(int n)
 {
 	int i = 0, ii;
 
-	while(i < n && n > 0)
+	while (i < n && n > 0)
 	{
 		ii = 0;
-		while(ii < n)
+		while (ii < n)
 		{
 			_putchar('#');
 			ii++;
