9b0ca09045edec2c00d72802e50af9987c9202ba
ermitse <ermiastse@gmail.com>
2022-09-15 16:44:34 -0700

    s

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, c;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-
+			c = j;
 			if (j > 9)
 			{
-				_putchar(j / 10);
-
+				_putchar('1');
+				c = j % 10;
 			}
-			_putchar((j % 10) + '0');
+			_putchar('0' + c);
 		}
 	_putchar('\n');
 	}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print-square - square
+ * print_square - square
  * @n: int
  * Return: no
  */
