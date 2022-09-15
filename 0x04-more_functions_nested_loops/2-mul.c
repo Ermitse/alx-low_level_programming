7b3296c6f2c7eb2597389be55291214180bfc686
ermitse <ermiastse@gmail.com>
2022-09-15 09:56:36 -0700

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies num
+ * @a @b: int in
+ * Return: ret value
+ */
+
+int mul(int a, int b)
+{
+	int ml;
+
+	ml = a * b;
+	return (ml);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints num
+ * Return: Always 0 
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0''\n');
+	}
+	return (0);
+}
