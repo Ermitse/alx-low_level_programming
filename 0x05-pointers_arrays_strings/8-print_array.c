a1c9a7463fd700e8cebf8565578e564171098e44
ermitse <ermiastse@gmail.com>
2022-09-19 12:15:16 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * print_array - entry
+ * @a: int pointer
+ * @n: int pointer
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] <= n; i++)
+	{
+		printf("%p", a[i]);
+		printf(", ");
+	}
+}
+
