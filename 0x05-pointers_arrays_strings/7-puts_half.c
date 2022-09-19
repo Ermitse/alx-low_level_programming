88aa0c21bcf5e9c0e27acfc372f61599cec8479f
ermitse <ermiastse@gmail.com>
2022-09-19 10:21:27 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * puts_halfputs_half - entry
+ * @str: char
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int j, k;
+
+	int count(int i)
+	{
+		for (i = 0; i <= str[i]; i++)
+		{
+		}
+		return (i);
+	}
+	j = count();
+	for (k = j / 2; k <= j; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
