28e0de377731ee8f8727deb7fa4716a9eead68ad
ermitse <ermiastse@gmail.com>
2022-09-19 09:03:42 -0700

    jump

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<string.h>
+/**
+ * puts2 - entry
+ * str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		if (i % 2 = 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
