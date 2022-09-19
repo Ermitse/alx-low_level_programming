4cc8b0554398d219ade7e4bb14e83575afbe13ce
ermitse <ermiastse@gmail.com>
2022-09-19 13:18:56 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * *_strcpy - entry
+ * @dest: char
+ * @@src: char
+ * Return: val
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] == '\0'; i++)
+	{
+		dest = src[i];
+	}
+	return (*dest);
+}
