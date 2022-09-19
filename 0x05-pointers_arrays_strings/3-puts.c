7c7c4f1c5c1914983eb1928576a084c158f28bb9
ermitse <ermiastse@gmail.com>
2022-09-19 07:42:04 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include<string.h>
+
 /**
- * _strlen - entry
- * @s: int pointer
+ * _puts - entry
+ * @str: char
  * Return: void
  */
 
-int _strlen(char *s)
+void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
+		_putchar(s[i]);
+		_putchar('\n');
 }
