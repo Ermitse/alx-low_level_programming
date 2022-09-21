83242e451644c7e287f675583b2bb0fc8d21ad62
ermitse <ermiastse@gmail.com>
2022-09-21 09:53:42 -0700

    fix 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include<string.h>
 /**
  * _strcat - string concatenate
  * @dest: char
@@ -17,7 +17,8 @@ char *_strcat(char *dest, char *src)
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
+		i++;
 	}
-	dest[i++] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
