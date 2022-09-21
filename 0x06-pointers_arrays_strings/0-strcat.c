904b4328d7827910cb7f6394fd98fbaa74a366c6
ermitse <ermiastse@gmail.com>
2022-09-21 08:21:35 -0700

    string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - string concatenate
+ * @dest: char
+ * @src: char
+ * Return: val
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] = '\0'; j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - string copy
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: val
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; src[i] = n; i++)
+	{
+		dest[] = src[i];
+	}
+	return (dest);
+}
