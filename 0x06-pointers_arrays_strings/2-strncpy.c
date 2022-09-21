4a6f2a75d4d443735f5d63bc29f2202db656d507
ermitse <ermiastse@gmail.com>
2022-09-21 11:09:19 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] = n; i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[] = src[i];
+		if (src[i] != '\0')
+		{
+		dest[i] = src[i];
+		}
+		else
+		dest[i] = '\n';
 	}
-	dest[] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
