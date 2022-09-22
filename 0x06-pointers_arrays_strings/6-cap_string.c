5d398efc1b189e8792c48c6ddb989b7a339328e7
ermitse <ermiastse@gmail.com>
2022-09-22 14:09:44 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int seps[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == separators[i])
+			if (*(s + count) == seps[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
@@ -27,5 +27,5 @@ char *cap_string(char *s)
 		}
 		count++;
 		}
-	return (x);
+	return (s);
 }
