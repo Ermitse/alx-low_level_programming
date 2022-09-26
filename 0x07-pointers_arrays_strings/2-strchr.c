d77ff490bd3d26363d230b4680e544e9127c4f67
ermitse <ermiastse@gmail.com>
2022-09-26 09:38:58 -0700

    b

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - check for string
+ * @s: char pointer to string
+ * @c: char val to check
+ * Return: returns val or null
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
