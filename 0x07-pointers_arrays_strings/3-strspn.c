a3938be17a6d8dcf0aea595320cb76d8c8f91feb
ermitse <ermiastse@gmail.com>
2022-09-26 12:01:23 -0700

    c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - locate char in str
+ * @s: char point
+ * @accept: contains list of char match wiz c
+ * Return: ret val
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			if (str2[j] == str1[i])
+			{
+				count++;
+				break;
+			}
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+	}
+	return (count);
+}
