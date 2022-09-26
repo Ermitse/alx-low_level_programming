9b606a182ebc50caf9ca7b2f7b1bbce7ea4e52bf
ermitse <ermiastse@gmail.com>
2022-09-26 12:49:46 -0700

    c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - check str
+ * @s: pointer str
+ * @accept: holds val
+ * Return: ret val
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (; *s != '\0'; s++)
+	{
+		for (i =0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+	}
+	return (0);
+}
