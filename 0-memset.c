d8e19b9f5371e4b3ebbc91ad066e11221532d16c
ermitse <ermiastse@gmail.com>
2022-09-26 08:29:42 -0700

    a

---
+++ 0-memset.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _memset - fills mem with constant val
+ * @s: char pointer
+ * @b: charval
+ * @n: int val
+ * Return: return val
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
