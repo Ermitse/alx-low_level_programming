f196275eccf4cc782da13c1dd220a4d61b4370dd
ermitse <ermiastse@gmail.com>
2022-09-28 14:19:29 -0700

    z

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,29 +7,30 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (!*s)
+	if (*s != '\0')
 	{
-	return (0);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
 /**
  * p - palidrome
  * @s: char poin
  * @l: int
+ * @i: int
  * Return: val
  */
-int p(char *s, int l)
+int p(char *s, int l, int i)
 {
-	if (l < 1)
+	if (i < l && s[i] == str[l])
 	{
-		return (1);
+		return (p(s, l - 1, i + 1));
 	}
-	if (*s == *(s + 1))
+	if (s[i] != s[l])
 	{
-		return (p(s + 1, l - 2));
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 /**
  * is_palindrome - palindrome
@@ -38,7 +39,8 @@ int p(char *s, int l)
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	int i = 0;
+	int len = _strlen_recursion(s) - 1;
 
-	return (p(s, len - 1));
+	return (p(s, len, i));
 }
