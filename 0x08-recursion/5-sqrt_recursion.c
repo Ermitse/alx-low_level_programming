a8c34210993f1c68aeb2ef13fad157fb00febb40
ermitse <ermiastse@gmail.com>
2022-09-28 11:56:29 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - sqrroot of n
+ * @n: int
+ * Return: ret val
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n == 1 && n == 0)
+		return (n);
+	else if (n < 0)
+		return (-1):
+
+	else if (i * i = n)
+		return (i);
+	else if (i * i != n)
+		return ((i + 1)(i + 1) = _sqrt_recursion(n));
+}
