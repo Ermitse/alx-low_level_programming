4016cd8d5e34ba4fddb762a8a60153fd4bfbab24
ermitse <ermiastse@gmail.com>
2022-09-30 15:17:17 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - entry
+ * @argc: count
+ * @argv: array arg
+ * Return: ret val
+ */
+int  main(int argc,char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - entr
+ * @argc: count
+ * @argv: array arg
+ * Return: rey val
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
