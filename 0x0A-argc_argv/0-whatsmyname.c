560fd55213375a344c2923d9f50176fb749aa3af
ermitse <ermiastse@gmail.com>
2022-09-30 14:56:30 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * @argc: count arg
+ * @argv: array arg
+ * Return: ret val
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - main entry
+ * @argc: count args
+ * @argv: vector arg
+ * Return: ret val
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
