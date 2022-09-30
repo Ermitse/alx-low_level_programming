e7659404215605516d679565b8f47848b0020149
ermitse <ermiastse@gmail.com>
2022-09-30 13:32:01 -0700

    a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
-#include<string.h>
-
+#include<unistd.h>
 /**
  * _puts - entry
  * @str: char
  * Return: void
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
