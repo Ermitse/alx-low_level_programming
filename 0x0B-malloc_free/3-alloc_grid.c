bf1c404a86022af1a895e14be3eef54dc4bc4cd4
ermitse <ermiastse@gmail.com>
2022-10-04 10:04:23 -0700

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * alloc_grid - alloc
+ * @width: 1d
+ * @height: 2d
+ * Return: ret val
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	char *m;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	m = malloc(sizeof(*m) * height);
+	if (m == 0)
+	{
+		free(m);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		m[i] = malloc(sizeof(*m) * width)
+			if (m[i] == 0)
+			{
+				for (i--; i >= 0; i--)
+				{
+					free(m[i]);
+					return (NULL);
+				}
+			}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			m[i][j] = 0;
+	return (m);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - grid
+ * @
+ */
+void free_grid(int **grid, int height)
+{
