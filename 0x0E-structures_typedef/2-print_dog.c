5108fc6fd23044f2540b8a94c0bbbfdf23824985
ermitse <ermiastse@gmail.com>
2022-10-10 12:38:15 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,12 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 		else
+		{
 			printf("Name: (nil)\n");
+		}
 		if (d->age)
 		{
-			printf("Age: %.6f\n", d->age);
+			printf("Age: %f\n", d->age);
 		}
 		else
 		{
