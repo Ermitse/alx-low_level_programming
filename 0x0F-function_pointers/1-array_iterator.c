694c0b74d075d75b7a076a994cb37359e8675557
ermitse <ermiastse@gmail.com>
2022-10-12 09:41:22 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - iterates array
+ * @array: array
+ * @size: size 
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for int
+ * @array: array to search 
+ * @size: size of array
+ * @cmp: point to comparing func
+ * Retrun: returns index of first int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
