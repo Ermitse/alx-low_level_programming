df29d98792e98dcb7f4582050474ea0e7e0b9eb6
ermitse <ermiastse@gmail.com>
2022-10-13 09:03:24 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 #include<stdarg.h>
 /**
  * sum_them_all - sum of n numb3rs
- * @n: number of int 
+ * @n: number of int
  * Return: ret sum
  */
 int sum_them_all(const unsigned int n, ...)
@@ -12,13 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	else
-	{
-		va_start (ap, n);
-		sum = 0;
-		for (i = 0; i < n; i++)
-			sum += va_arg (ap, int);
-		va_end (ap);
-	}
+
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+
 	return (sum);
 }
