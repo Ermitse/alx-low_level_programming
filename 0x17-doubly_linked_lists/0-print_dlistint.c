5e66248501208f8ebb9baca3e765062db129dd35
ermitse <ermiastse@gmail.com>
2022-12-08 09:44:14 -0800

    fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,14 +4,14 @@
  * @h: head
  * Return: num of nodes
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
 
 	while(h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->n;
+		h = h->next;
 		num++;
 	}
 	return (num);
