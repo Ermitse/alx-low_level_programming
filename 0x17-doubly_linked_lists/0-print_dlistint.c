c670822d138a3977e2dbf46b1a2800934907cd5f
ermitse <ermiastse@gmail.com>
2022-12-08 09:30:55 -0800

    first

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/*
+ * print_distinct - prints elements
+ * @h: head
+ * Return: num of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t num = 0;
+
+	while(h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->n;
+		num++;
+	}
+	return (num);
+}
