cb273fc7ff23ca0c75096b07a590d8a689520106
ermitse <ermiastse@gmail.com>
2022-12-08 12:58:27 -0800

    inset fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,6 +4,7 @@
  * @h: pointer of head
  * @idx: index position for a new node
  * @n: integer value of node * Return: adress of new node else NULL
+ * Return: ret val
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
